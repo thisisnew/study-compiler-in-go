e4bbf78449e62afcfefe117ee53883b9ee46745a
thisisnew <thisisnew@naver.com>
2021-11-07 23:43:35 +0900

    2장 Hello ByteCode 첫번째 명령어 컴파일러파일생성

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,1 +1,11 @@
 package compiler
+
+import (
+	"study-compiler-in-go/code"
+	"study-compiler-in-go/object"
+)
+
+type Compiler struct {
+	instructions code.Instructions
+	constants    []object.Object
+}
