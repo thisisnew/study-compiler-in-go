5bd9865d6d7708dcfef89c8f6f024a581c8c4a56
thisisnew <thisisnew@naver.com>
2021-11-09 22:12:47 +0900

    2장 Hello ByteCode 첫번째 명령어

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"study-compiler-in-go/ast"
 	"study-compiler-in-go/code"
 	"study-compiler-in-go/object"
 )
@@ -8,4 +9,27 @@ import (
 type Compiler struct {
 	instructions code.Instructions
 	constants    []object.Object
+}
+
+func New() *Compiler {
+	return &Compiler{
+		instructions: code.Instructions{},
+		constants:    []object.Object{},
+	}
+}
+
+func (c *Compiler) Compile(node ast.Node) error {
+	return nil
+}
+
+func (c *Compiler) Bytecode() *Bytecode {
+	return &Bytecode{
+		Instruction: c.instructions,
+		Constants:   c.constants,
+	}
+}
+
+type Bytecode struct {
+	Instruction code.Instructions
+	Constants   []object.Object
 }
